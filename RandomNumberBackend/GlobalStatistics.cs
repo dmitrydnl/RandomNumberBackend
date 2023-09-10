842450c28052b9145dc3224dd464748a587bb49b
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-10 18:39:23 +0300

    GlobalStatistics

---
+++ RandomNumberBackend/GlobalStatistics.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RandomNumberBackend.Database;
+
+namespace RandomNumberBackend
+{
+    public class GlobalStatistics
+    {
+        private readonly IDatabase database;
+
+        public GlobalStatistics(IDatabase database)
+        {
+            this.database = database;
+        }
+
+        [FunctionName("GlobalStatistics")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("");
+        }
+    }
+}
