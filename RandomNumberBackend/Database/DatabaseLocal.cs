7d8a169098ded936d72d61a812970a43d9a87772
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-10 17:19:02 +0300

    create Database

---
+++ RandomNumberBackend/Database/DatabaseLocal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RandomNumberBackend.Database
+{
+    public class DatabaseLocal : IDatabase
+    {
+        public bool IsGameExist(string nickname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateGame(string nickname, int hiddenNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RandomNumberBackend/Database/IDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RandomNumberBackend.Database
+{
+    public interface IDatabase
+    {
+        public bool IsGameExist(string nickname);
+
+        public bool CreateGame(string nickname, int hiddenNumber);
+    }
+}
