6f65d4ad4ac4468730472bb0574b8dfbe3a57643
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-10 16:58:34 +0300

    create NumberGenerator

---
+++ RandomNumberBackend/Game/NumberGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomNumberBackend.Game
+{
+    public class NumberGenerator
+    {
+        private readonly Random random;
+        private readonly int min;
+        private readonly int max;
+
+        public NumberGenerator(int min, int max)
+        {
+            random = new Random();
+            this.min = min;
+            this.max = max;
+        }
+
+        public int Generate()
+        {
+            return random.Next(min, max + 1);
+        }
+    }
+}
