aeb130cffbe0fda202fc92c92c4d49b458794bf6
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-10 16:50:20 +0300

    Create StartGame.cs

---
+++ RandomNumberBackend/StartGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+using System.Security.Cryptography;
+
+namespace RandomNumberBackend
+{
+    public static class StartGame
+    {
+        [FunctionName("StartGame")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string nickname = req.Query["nickname"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            nickname = nickname ?? data?.nickname;
+
+            if (string.IsNullOrEmpty(nickname))
+            {
+                return new BadRequestResult();
+            }
+
+            int hiddenNumber = 45;
+
+            string responseMessage = $"{nickname} your hidden number is {hiddenNumber}.";
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
